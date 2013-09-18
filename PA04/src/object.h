8d7c5a1b12a58944ed50e09d35d5cd42ba883095
Liesl Wigand <lieslw@robotics5-iMac.(none)>
2013-09-17 17:15:23 -0700

    updated object class for loading.

--- PA04/src/object.h
+++ PA04/src/object.h
@@ -1,15 +1,25 @@
 /*
 Storage for objects: buffer id, model matrix
 */
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Object {
   private:
     glm::mat4 model;
     GLuint geometry;
+    std::vector < glm::vec3 > out_vertices;
+    std::vector < glm::vec2 > out_uvs;
+    std::vector < glm::vec3 > out_normals;
     
   public:
-    Object();
+    Object(const char* filename);
     ~Object();
+    bool loadObject();
     glm::mat4 getModel();
     GLuint getGeometry();
 
