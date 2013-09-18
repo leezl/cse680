5bf5df409252215bf730d5d201dd8cfb1c53f4ea
Liesl Wigand <wolfdrake15@gmail.com>
2013-09-17 21:00:14 -0700

    fixed a declaration, then typed python code by accident. Time for bed.

--- PA04/src/object.h
+++ PA04/src/object.h
@@ -19,7 +19,10 @@ class Object {
   public:
     Object(const char* filename);
     ~Object();
-    bool loadObject();
+    bool loadObject((const char * path,
+                        std::vector < glm::vec3 > & out_vertices,
+                        std::vector < glm::vec2 > & out_uvs,
+                        std::vector < glm::vec3 > & out_normals);
     glm::mat4 getModel();
     GLuint getGeometry();
 
