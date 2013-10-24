848bdf2bcb141b52a549091c5538e556d9978929
Liesl <wolfdrake15@gmail.com>
2013-10-24 11:48:49 -0700

    Update main.cpp

--- PA02/src/main.cpp
+++ PA02/src/main.cpp
@@ -281,7 +281,7 @@ bool initialize() {
                           {{-1.0, 1.0, 1.0}, {0.0, 1.0, 1.0}},
                           {{1.0, -1.0, 1.0}, {1.0, 0.0, 1.0}}
                         };
-    std::vector<int> indices = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
+    std::vector<unsigned int> indices = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
     elementSize = indices.size();
 
     // Create a Vertex Buffer object to store this vertex info on the GPU
